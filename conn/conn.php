cf822ec0d058fe0777a13d987a30829b98008e12
Kakoi1 <125711019+Kakoi1@users.noreply.github.com>
2024-09-23 17:48:11 +0800

    Update conn.php

--- conn/conn.php
+++ conn/conn.php
@@ -1,19 +1,16 @@
 <?php
-function connectDB()
-{
-    $host = getenv('DB_HOST'); 
-    $dbname = getenv('DB_DATABASE');
-    $username = getenv('DB_USERNAME');
-    $password = getenv('DB_PASSWORD');
 
-    try {
-        $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $conn;
-    } catch (PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-    }
+$host = getenv('DB_HOST'); 
+$dbname = getenv('DB_DATABASE');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
 
-    return null;
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; // Optional: for debugging connection
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
-?>>
+
+?>
